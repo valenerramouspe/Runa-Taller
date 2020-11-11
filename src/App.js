d7206c813f0f1efb71e3903830f9818bda7a208e
valenerramouspe <valen_erramouspe@hotmail.com>
2020-11-12 00:53:06 +0100

    added router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Link } from "react-router-dom";
 import Home from "./Components/Home";
 
-function App() {
-  return <Home />;
+export default function App(props) {
+  return (
+    <div>
+      <Link to="/home">
+        <img id="logoHome" src="images/Logo-home.png" />
+      </Link>
+      {props.children}
+    </div>
+  );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Home from "./Components/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+      <Switch>
+        <Route path="/" component={Home}></Route>
+        {/* <Route path="galeria" component={Proyectos} />
+        <Route path="nosotras" component={Nosotras} /> */}
+      </Switch>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//
